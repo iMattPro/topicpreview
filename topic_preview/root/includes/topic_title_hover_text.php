3e57e92394e65e5d638e974b4d59437f0cc9a71a
Matt <maf675@gmail.com>
2010-06-13 18:44:38 -0700

    Commit version 1.0.2

--- topic_preview/root/includes/topic_title_hover_text.php
+++ topic_preview/root/includes/topic_title_hover_text.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB3
-* @version $Id: topic_title_hover_text.php, v 1.0.0 2010/1/31 21:54:52 VSE Exp $
+* @version $Id: topic_title_hover_text.php, 3 2010/2/04 15:14:42 VSE Exp $
 * @copyright (c) Matt Friedman
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
@@ -19,6 +19,7 @@ if (!defined('IN_PHPBB'))
 /**
 * Include only once.
 * This function originally written by RMcGirr83 for his Topic Text Hover Mod
+* Modified by Matt Friedman to display smileys as text
 */
 if(!function_exists('bbcode_strip'))
 {
@@ -26,16 +27,18 @@ if(!function_exists('bbcode_strip'))
 	{
 			static $RegEx = array();
 			static $bbcode_strip = 'flash';
+			// Save the smileys - show them as text
+			$text = smiley_text($text, true);
 			// html is pretty but it may break the layout of the tooltip...let's
 			// remove some common ones from the tip
-			$text_html = array('&quot;','&amp;','&#039;','&lt;','&gt;');
-			$text = str_replace($text_html,'',$text);
+//			$text_html = array('&quot;','&amp;','&#039;','&lt;','&gt;');
+//			$text = str_replace($text_html,'',$text);
 			if (empty($RegEx))
 			{
-				$RegEx = array('`<[^>]*>(.*<[^>]*>)?`Usi', // HTML code
-					'`\[(' . $bbcode_strip . ')[^\[\]]+\].*\[/(' . $bbcode_strip . ')[^\[\]]+\]`Usi', // bbcode to strip
-					'`\[/?[^\[\]]+\]`mi', // Strip all bbcode tags
-					'`[\s]+`' // Multiple spaces
+				$RegEx = array('#<[^>]*>(.*<[^>]*>)?#Usi', // HTML code
+					'#\[(' . $bbcode_strip . ')[^\[\]]+\].*\[/(' . $bbcode_strip . ')[^\[\]]+\]#Usi', // bbcode to strip
+					'#\[/?[^\[\]]+\]#mi', // Strip all bbcode tags
+					'#[\s]+#' // Multiple spaces
 				);
 			}
 		return preg_replace($RegEx, ' ', $text );
