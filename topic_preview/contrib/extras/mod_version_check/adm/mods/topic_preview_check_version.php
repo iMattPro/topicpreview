9dff8f3b470094d2fea3c903f5dc6e0895dadc96
Matt Friedman <maf675@gmail.com>
2012-09-03 00:24:56 -0700

    Update MOD to 1.1.0 and phpBB target to 3.0.11

--- topic_preview/contrib/extras/mod_version_check/adm/mods/topic_preview_check_version.php
+++ topic_preview/contrib/extras/mod_version_check/adm/mods/topic_preview_check_version.php
@@ -25,7 +25,7 @@ class topic_preview_check_version
 			'author'	=> 'VSE',
 			'title'		=> 'Topic Preview',
 			'tag'		=> 'topic_preview',
-			'version'	=> '1.0.10',
+			'version'	=> '1.1.0',
 			'file'		=> array('orcamx.vlexofree.com', 'software', 'mods.xml'),
 		);
 	}
--- topic_preview/root/install_topic_preview.php
+++ topic_preview/root/install_topic_preview.php
@@ -17,7 +17,6 @@ $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
-// Start session management
 $user->session_begin();
 $auth->acl($user->data);
 $user->setup();
@@ -27,28 +26,10 @@ if (!file_exists($phpbb_root_path . 'umil/umil_auto.' . $phpEx))
 	trigger_error('Please download the latest UMIL (Unified MOD Install Library) from: <a href="http://www.phpbb.com/mods/umil/">phpBB.com/mods/umil</a>', E_USER_ERROR);
 }
 
-// The name of the mod to be displayed during installation.
 $mod_name = 'TP_MOD';
-
-/*
-* The name of the config variable which will hold the currently installed version
-* UMIL will handle checking, setting, and updating the version itself.
-*/
 $version_config_name = 'topic_preview_version';
-
-/*
-* The language file which will be included when installing
-* Language entries that should exist in the language file for UMIL (replace $mod_name with the mod's name you set to $mod_name above)
-*/
 $language_file = 'mods/topic_preview';
 
-/*
-* The array of versions and actions within each.
-* You do not need to order it a specific way (it will be sorted automatically), however, you must enter every version, even if no actions are done for it.
-*
-* You must use correct version numbering.  Unless you know exactly what you can use, only use X.X.X (replacing X with an integer).
-* The version numbering must otherwise be compatible with the version_compare function - http://php.net/manual/en/function.version-compare.php
-*/
 $versions = array(
 	// Version 1.0.6
 	'1.0.6'	=> array(
@@ -65,23 +46,19 @@ $versions = array(
 	),
 
 	// Version 1.0.7
-	'1.0.7' => array(
-		// Nothing changed in this version.
-	),
+	'1.0.7' => array(),
 
 	// Version 1.0.9
-	'1.0.9' => array(
-		// Nothing changed in this version.
-	),
+	'1.0.9' => array(),
 
 	// Version 1.0.10
-	'1.0.10' => array(
-		// Nothing changed in this version.
-	),
+	'1.0.10' => array(),
+
+	// Version 1.1.0
+	'1.1.0' => array(),
 
 );
 
-// Include the UMIF Auto file and everything else will be handled automatically.
 include($phpbb_root_path . 'umil/umil_auto.' . $phpEx);
 
 ?>
