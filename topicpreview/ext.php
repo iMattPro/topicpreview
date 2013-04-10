54d12009ec48700a52351d932f66727f664cf96d
Matt Friedman <maf675@gmail.com>
2013-04-10 15:50:53 -0700

    Tweak ext doc block

--- topicpreview/ext.php
+++ topicpreview/ext.php
@@ -16,7 +16,7 @@ if (!defined('IN_PHPBB'))
 }
 
 /**
- * Main extension class for topicpreview extension.
+ * Main extension class for this extension.
  */
 class phpbb_ext_vse_topicpreview_ext extends phpbb_extension_base
 {
