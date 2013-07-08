e7e764fc03c9b2e3d2aa1a11747ff3199c6ad889
Matt Friedman <maf675@gmail.com>
2013-07-07 22:57:32 -0700

    Add drift animation during fadein-fadeout

--- topic_preview/root/styles/topic_preview/topic_preview.js
+++ topic_preview/root/styles/topic_preview/topic_preview.js
@@ -15,8 +15,9 @@
 			"style" : "light",
 			"width" : 320,
 			"delay" : 500,
-			"left" : 0,
-			"top"  : 12
+			"left"  : 0,
+			"top"   : 12,
+			"drift" : 12
 		}, options );
 
 		var previewContainer = $('<div id="topic_preview" class="' + settings.style + '"><div id="topic_preview_inner"></div><div id="topic_preview_pointer"><div id="topic_preview_pointer_inner"></div></div></div>').appendTo("body");
@@ -56,9 +57,9 @@
 						.html(firstPostText);
 
 					// Handle window top edge detection, and invert topic preview if needed 
-					var previewTop = obj.offset().top - previewContainer.height() - settings.top;
+					var previewTop = obj.offset().top - previewContainer.height() - settings.top + settings.drift;
 					$("#topic_preview_pointer, #topic_preview_pointer_inner").toggleClass("invert", topEdgeDetect(previewTop));
-					previewTop = topEdgeDetect(previewTop) ? obj.offset().top + (settings.top * 3) : previewTop;
+					previewTop = topEdgeDetect(previewTop) ? obj.offset().top + (settings.top * 3) + settings.drift: previewTop;
 
 					// position the topic preview relative to the hover object
 					previewContainer
@@ -67,7 +68,10 @@
 							"top"   : (previewTop + "px"),
 							"left"  : ((obj.offset().left + settings.left) + "px")
 						})
-						.fadeIn("fast"); // display the topic preview with a fadein
+						.fadeIn("fast") // display the topic preview with a fadein
+						.animate({'top': "-=" + settings.drift + "px"}, {duration: "fast", queue: false}, function() {
+							// animation complete
+						});
 				}, settings.delay); // Use a delay before fading in topic preview
 
 			}, function() {
@@ -76,8 +80,10 @@
 					clearTimeout(previewTimeout); // clear any existing timeouts
 				}
 
-				previewContainer.stop(true, true).fadeOut("fast"); // hide the topic preview with a fadeout
-
+				previewContainer.stop(true, true).fadeOut("fast") // hide the topic preview with a fadeout
+					.animate({'top': "-=" + settings.drift + "px"}, {duration: "fast", queue: false}, function() {
+						// animation complete
+					});
 			});
 		});
 	};
