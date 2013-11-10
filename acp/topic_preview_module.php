688035bd5fd036576b1570b5339f02579a54441a
Matt Friedman <maf675@gmail.com>
2013-11-09 23:42:20 -0800

    Refactor acp module

--- acp/topic_preview_module.php
+++ acp/topic_preview_module.php
@@ -14,16 +14,31 @@ namespace vse\topicpreview\acp;
 */
 class topic_preview_module
 {
-	var $u_action;
+	public $u_action;
 
-	function main($id, $mode)
+	protected $db;
+	protected $cache;
+	protected $user;
+	protected $template;
+	protected $request;
+	protected $config;
+	protected $phpbb_root_path;
+
+	public function main($id, $mode)
 	{
-		global $db, $user, $auth, $template, $cache;
-		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
+		global $db, $cache, $user, $template, $request, $config, $phpbb_root_path;
 
-		$user->add_lang('acp/common');
+		$this->db = $db;
+		$this->cache = $cache;
+		$this->user = $user;
+		$this->template = $template;
+		$this->request = $request;
+		$this->config = $config;
+		$this->phpbb_root_path = $phpbb_root_path;
+
+		$this->user->add_lang('acp/common');
 		$this->tpl_name = 'acp_topic_preview';
-		$this->page_title = $user->lang['TOPIC_PREVIEW'];
+		$this->page_title = $this->user->lang['TOPIC_PREVIEW'];
 
 		$form_key = 'acp_topic_preview';
 		add_form_key($form_key);
@@ -37,27 +52,27 @@ class topic_preview_module
 				trigger_error('FORM_INVALID');
 			}
 
-			$topic_preview_limit = request_var('topic_preview_limit', 0);
+			$topic_preview_limit = $this->request->variable('topic_preview_limit', 0);
 			set_config('topic_preview_limit', abs($topic_preview_limit));
 
-			$topic_preview_strip_bbcodes = request_var('topic_preview_strip_bbcodes', '');
+			$topic_preview_strip_bbcodes = $this->request->variable('topic_preview_strip_bbcodes', '');
 			set_config('topic_preview_strip_bbcodes', $topic_preview_strip_bbcodes);
 
-			$topic_preview_avatars = request_var('topic_preview_avatars', 0);
+			$topic_preview_avatars = $this->request->variable('topic_preview_avatars', 0);
 			set_config('topic_preview_avatars', $topic_preview_avatars);
 
-			$topic_preview_last_post = request_var('topic_preview_last_post', 0);
+			$topic_preview_last_post = $this->request->variable('topic_preview_last_post', 0);
 			set_config('topic_preview_last_post', $topic_preview_last_post);
 
-			trigger_error($user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action));
+			trigger_error($this->user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action));
 		}
 
-		$template->assign_vars(array(
-			'TOPIC_PREVIEW_LIMIT'		=> isset($config['topic_preview_limit']) ? $config['topic_preview_limit'] : '',
-			'TOPIC_PREVIEW_STRIP'		=> isset($config['topic_preview_strip_bbcodes']) ? $config['topic_preview_strip_bbcodes'] : '',
-			'S_TOPIC_PREVIEW_AVATARS'	=> isset($config['topic_preview_avatars']) ? $config['topic_preview_avatars'] : false,
-			'S_TOPIC_PREVIEW_LAST_POST'	=> isset($config['topic_preview_last_post']) ? $config['topic_preview_last_post'] : false,
-			'TOPIC_PREVIEW_VERSION'		=> isset($config['topic_preview_version']) ? 'v' . $config['topic_preview_version'] : '',
+		$this->template->assign_vars(array(
+			'TOPIC_PREVIEW_LIMIT'		=> isset($this->config['topic_preview_limit']) ? $this->config['topic_preview_limit'] : '',
+			'TOPIC_PREVIEW_STRIP'		=> isset($this->config['topic_preview_strip_bbcodes']) ? $this->config['topic_preview_strip_bbcodes'] : '',
+			'S_TOPIC_PREVIEW_AVATARS'	=> isset($this->config['topic_preview_avatars']) ? $this->config['topic_preview_avatars'] : false,
+			'S_TOPIC_PREVIEW_LAST_POST'	=> isset($this->config['topic_preview_last_post']) ? $this->config['topic_preview_last_post'] : false,
+			'TOPIC_PREVIEW_VERSION'		=> isset($this->config['topic_preview_version']) ? 'v' . $this->config['topic_preview_version'] : '',
 			'U_ACTION'					=> $this->u_action,
 		));
 	}
