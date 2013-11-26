0d364aeac203c419cc8057bb524a450b9d18ade1
Matt Friedman <maf675@gmail.com>
2013-11-25 16:17:17 -0800

    Update info cat

--- acp/topic_preview_info.php
+++ acp/topic_preview_info.php
@@ -21,7 +21,7 @@ class topic_preview_info
 			'title'		=> 'TOPIC_PREVIEW',
 			'version'	=> '2.1.0',
 			'modes'		=> array(
-				'settings'	=> array('title' => 'TOPIC_PREVIEW_SETTINGS', 'auth' => 'acl_a_board', 'cat' => array('TOPIC_PREVIEW_SETTINGS')),
+				'settings'	=> array('title' => 'TOPIC_PREVIEW_SETTINGS', 'auth' => 'acl_a_board', 'cat' => array('TOPIC_PREVIEW')),
 			),
 		);
 	}
