d04a42dec497ea04de1bde36d8866c24873c2cb8
Matt Friedman <maf675@gmail.com>
2014-03-01 19:46:32 -0800

    Add more to the functional test

--- tests/functional/topic_preview_test.php
+++ tests/functional/topic_preview_test.php
@@ -31,11 +31,11 @@ class phpbb_functional_topic_preview_test extends extension_functional_test_case
 		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
 		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
 
-		// Test creating a reply with bbcode
-// 		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
-// 
-// 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
-// 		$this->assertContains('This is a test post posted by the testing framework.', $crawler->filter('html')->text());
+		// Test creating a post with smiley
+		$post2 = $this->create_topic(2, 'Test Topic 2', 'This is another test topic :) posted by the testing framework.');
+
+		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
+		$this->assertContains('This is another test topic :) posted by the testing framework.', $crawler->filter('html')->text());
 
 		// Test quoting a message
 // 		$crawler = self::request('GET', "posting.php?mode=quote&f=2&t={$post2['topic_id']}&p={$post2['post_id']}&sid={$this->sid}");
