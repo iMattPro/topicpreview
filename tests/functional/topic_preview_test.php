d816b4b4aa7ac479346befff09583ec269cf4d45
Matt Friedman <maf675@gmail.com>
2014-03-01 19:52:22 -0800

    Start with simple test of posted topic

--- tests/functional/topic_preview_test.php
+++ tests/functional/topic_preview_test.php
@@ -21,21 +21,24 @@ class phpbb_functional_topic_preview_test extends extension_functional_test_case
 		$this->enable_extension();
 	}
 
-	public function test_post_new_topic()
+	public function test_preview_new_topic()
 	{
 		$this->login();
 
 		// Test creating topic
 		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
 
-		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
-		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
+		$crawler = self::request('GET', 'viewforum.php?f=2');
+		$this->assertGreaterThan(0, $crawler->filter('.topiclist')->count());
+
+//		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
+//		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
 
 		// Test creating a post with smiley
-		$post2 = $this->create_topic(2, 'Test Topic 2', 'This is another test topic :) posted by the testing framework.');
+//		$post2 = $this->create_topic(2, 'Test Topic 2', 'This is another test topic :) posted by the testing framework.');
 
-		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
-		$this->assertContains('This is another test topic :) posted by the testing framework.', $crawler->filter('html')->text());
+//		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
+//		$this->assertContains('This is another test topic :) posted by the testing framework.', $crawler->filter('html')->text());
 
 		// Test quoting a message
 // 		$crawler = self::request('GET', "posting.php?mode=quote&f=2&t={$post2['topic_id']}&p={$post2['post_id']}&sid={$this->sid}");
