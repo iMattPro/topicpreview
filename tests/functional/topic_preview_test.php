d407a1fc696b37a250dc0233ff4414f2e6c77297
Matt Friedman <maf675@gmail.com>
2014-03-01 19:43:50 -0800

    Add a topic preview functional test

---
+++ tests/functional/topic_preview_test.php
@@ -0,0 +1,44 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 Matt Friedman
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @group functional
+*/
+class phpbb_functional_topic_preview_test extends extension_functional_test_case
+{
+	public function setUp()
+	{
+		parent::setUp();
+		$this->login();
+		$this->admin_login();
+		$this->set_extension('vse', 'topicpreview', 'Topic Preview');
+		$this->enable_extension();
+	}
+
+	public function test_post_new_topic()
+	{
+		$this->login();
+
+		// Test creating topic
+		$post = $this->create_topic(2, 'Test Topic 1', 'This is a test topic posted by the testing framework.');
+
+		$crawler = self::request('GET', "viewforum.php?f=2&sid={$this->sid}");
+		$this->assertContains('This is a test topic posted by the testing framework.', $crawler->filter('html')->text());
+
+		// Test creating a reply with bbcode
+// 		$post2 = $this->create_post(2, $post['topic_id'], 'Re: Test Topic 1', 'This is a test [b]post[/b] posted by the testing framework.');
+// 
+// 		$crawler = self::request('GET', "viewtopic.php?t={$post2['topic_id']}&sid={$this->sid}");
+// 		$this->assertContains('This is a test post posted by the testing framework.', $crawler->filter('html')->text());
+
+		// Test quoting a message
+// 		$crawler = self::request('GET', "posting.php?mode=quote&f=2&t={$post2['topic_id']}&p={$post2['post_id']}&sid={$this->sid}");
+// 		$this->assertContains('This is a test post posted by the testing framework.', $crawler->filter('html')->text());
+	}
+}
