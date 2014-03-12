e2c9ba3a530e67721c12c189188cd931975f87b8
Matt Friedman <maf675@gmail.com>
2014-03-11 21:48:13 -0700

    Fix name of test classes

--- tests/functional/topic_preview_acp_test.php
+++ tests/functional/topic_preview_acp_test.php
@@ -10,7 +10,7 @@
 /**
 * @group functional
 */
-class phpbb_functional_topic_preview_acp_test extends extension_functional_test_case
+class extension_functional_topic_preview_acp_test extends extension_functional_test_case
 {
 	public function setUp()
 	{
