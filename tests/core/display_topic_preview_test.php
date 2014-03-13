cbb7037285b2e8db7d6fd019be0ffbf9605498e0
Matt Friedman <maf675@gmail.com>
2014-03-12 22:44:55 -0700

    Add core unit tests

---
+++ tests/core/display_topic_preview_test.php
@@ -0,0 +1,94 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace vse\topicpreview\tests\core;
+
+class display_topic_preview_test extends topic_preview_base
+{
+	public static function topic_preview_display_data()
+	{
+		return array(
+			array(
+				array(
+					'first_post_text' => 'First message',
+					'first_poster_avatar' => '',
+					'first_poster_avatar_type' => 0,
+					'topic_first_post_id' => 1,
+					'last_post_text' => '',
+					'last_poster_avatar' => '',
+					'last_poster_avatar_type' => 0,
+					'topic_last_post_id' => 1,
+				),
+				array(
+					'TOPIC_PREVIEW_FIRST_POST'		=> 'First message',
+					'TOPIC_PREVIEW_FIRST_AVATAR'	=> '',
+					'TOPIC_PREVIEW_LAST_POST'		=> '',
+					'TOPIC_PREVIEW_LAST_AVATAR'		=> '',
+				),
+			),
+			array(
+				array(
+					'first_post_text' => 'Second message [b:3o8ohvlj]with bold text[/b:3o8ohvlj] <!-- s:) --><img src="{SMILIES_PATH}/icon_e_smile.gif" alt=":)" title="Smile" /><!-- s:) --> and smiley',
+					'first_poster_avatar' => '',
+					'first_poster_avatar_type' => 0,
+					'topic_first_post_id' => 2,
+					'last_post_text' => str_repeat ('a', 155),
+					'last_poster_avatar' => '',
+					'last_poster_avatar_type' => 0,
+					'topic_last_post_id' => 3,
+				),
+				array(
+					'TOPIC_PREVIEW_FIRST_POST'		=> 'Second message with bold text :) and smiley',
+					'TOPIC_PREVIEW_FIRST_AVATAR'	=> '',
+					'TOPIC_PREVIEW_LAST_POST'		=> str_repeat ('a', 150) . '...',
+					'TOPIC_PREVIEW_LAST_AVATAR'		=> '',
+				),
+			),
+			array(
+				array(
+					'first_post_text' => 'Third message with <!-- m --><a class="postlink" href="http://google.com">http://google.com</a><!-- m --> magic url and <!-- e --><a href="mailto:test@google.com">test@google.com</a><!-- e --> email',
+					'first_poster_avatar' => '',
+					'first_poster_avatar_type' => 0,
+					'topic_first_post_id' => 4,
+					'last_post_text' => str_repeat ('a', 155),
+					'last_poster_avatar' => '',
+					'last_poster_avatar_type' => 0,
+					'topic_last_post_id' => 5,
+				),
+				array(
+					'TOPIC_PREVIEW_FIRST_POST'		=> 'Third message with magic url and test@google.com email',
+					'TOPIC_PREVIEW_FIRST_AVATAR'	=> '',
+					'TOPIC_PREVIEW_LAST_POST'		=> str_repeat ('a', 150) . '...',
+					'TOPIC_PREVIEW_LAST_AVATAR'		=> '',
+				),
+			),
+		);
+	}
+
+	/**
+	* @dataProvider topic_preview_display_data
+	*/
+	public function test_display_topic_preview($data, $expected)
+	{
+		// Start with an empty block array
+		$block = array();
+
+		// Get an instance of topic preview class
+		$topic_preview_manager = $this->topic_preview_manager();
+
+		// Make sure setup has been run
+		$topic_preview_manager->setup();
+
+		// Update the block array with topic preview data
+		$block = $topic_preview_manager->display_topic_preview($data, $block);
+
+		// Test that we get the expected result
+		$this->assertEquals($expected, $block);
+	}
+}
---
+++ tests/core/topic_preview_base.php
@@ -0,0 +1,69 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace vse\topicpreview\tests\core;
+
+require_once dirname(__FILE__) . '/../../../../../includes/functions_content.php';
+require_once dirname(__FILE__) . '/../../../../../includes/utf/utf_tools.php';
+
+class topic_preview_base extends \extension_database_test_case
+{
+	protected $config;
+	protected $db;
+	protected $request;
+	protected $template;
+	protected $user;
+	protected $root_path;
+
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/topic_preview.xml');
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		global $config, $user, $phpbb_path_helper, $phpbb_root_path, $phpEx;
+
+		$this->db = $this->new_dbal();
+
+		$config = $this->config = new \phpbb\config\config(array(
+			'topic_preview_limit'		=> 150,
+			'topic_preview_avatars'		=> 1,
+			'topic_preview_last_post'	=> 1,
+			'allow_avatar'				=> 1,
+			));
+		set_config(null, null, null, $config);
+
+		$this->request = $this->getMock('\phpbb\request\request');
+
+		$user = $this->user = $this->getMock('\phpbb\user');
+		$this->user->style['style_path'] = 'prosilver';
+		$this->user->data['user_topic_preview'] = 1;
+
+		$phpbb_path_helper = new \phpbb\path_helper(
+			new \phpbb\symfony_request(
+				new \phpbb_mock_request()
+			),
+			new \phpbb\filesystem(),
+			$phpbb_root_path,
+			$phpEx
+		);
+
+		$this->template = new \phpbb\template\twig\twig($phpbb_path_helper, $this->config, $this->user, new \phpbb\template\context());
+
+		$this->root_path = $phpbb_root_path;
+	}
+
+	protected function topic_preview_manager()
+	{
+		return new \vse\topicpreview\core\topic_preview($this->config, $this->db, $this->request, $this->template, $this->user, $this->root_path);
+	}
+}
