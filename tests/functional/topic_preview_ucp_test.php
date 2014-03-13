9c3cd7721f25481d83a40849662530b0623430b7
Matt Friedman <maf675@gmail.com>
2014-03-13 09:00:12 -0700

    Add a test for the UCP page

---
+++ tests/functional/topic_preview_ucp_test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 Matt Friedman
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @group functional
+*/
+class extension_functional_topic_preview_ucp_test extends extension_functional_test_case
+{
+	public function setUp()
+	{
+		parent::setUp();
+		$this->login();
+		$this->admin_login();
+		$this->set_extension('vse', 'topicpreview', 'Topic Preview');
+		$this->enable_extension();
+	}
+
+	public function test_ucp_pages($mode)
+	{
+		//$this->add_lang_ext('info_acp_topic_preview');
+
+		$crawler = self::request('GET', 'ucp.php?i=ucp_prefs&mode=view&sid=' . $this->sid);
+		$this->assertContains('Display topic previews', $crawler->filter('#cp-main')->text());
+		//$this->assertContains($this->lang('TOPIC_PREVIEW_DISPLAY'), $crawler->filter('#cp-main')->text());
+	}
+}
