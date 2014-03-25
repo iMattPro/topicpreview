aa8a7f81cee9d8b29ec3c376789fd24f576add96
cybernet <cybernet2u@yahoo.com>
2014-03-25 23:32:19 +0200

    lang RO by cybernet :)
    rev1

--- language/ro/info_acp_topic_preview.php
+++ language/ro/info_acp_topic_preview.php
@@ -35,12 +35,12 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'TOPIC_PREVIEW'				=> 'Previzualizare Subiecte',
-	'TOPIC_PREVIEW_EXPLAIN'			=> 'Va arăta o parte din textul primului subiect într-un tooltip în timp ce eşti cu mausul deasupra titlului subiectului.',
-	'TOPIC_PREVIEW_SETTINGS'		=> 'Topic preview settings',
+	'TOPIC_PREVIEW'				=> 'Previzualizare Subiect',
+	'TOPIC_PREVIEW_EXPLAIN'			=> 'Va arăta o parte din textul primului subiect într-un tooltip în timp ce eşti cu mouse-ul deasupra titlului subiectului.',
+	'TOPIC_PREVIEW_SETTINGS'		=> 'Setari pentru previzualizarea subiectelor',
 	'TOPIC_PREVIEW_DISPLAY'			=> 'Afişează previzualizările subiectelor',
 	'TOPIC_PREVIEW_LENGTH'			=> 'Lungimea textului subiectelor previzualizate',
-	'TOPIC_PREVIEW_LENGTH_EXPLAIN'		=> 'Introdu numărul de caractere ce vor fi afişate în tooltip (valoarea implicită este 150). Setarea acestei valori la 0 scoate din funcţiune această particularitate.',
+	'TOPIC_PREVIEW_LENGTH_EXPLAIN'		=> 'Introdu numărul de caractere ce vor fi afişate în tooltip ( valoarea implicită este 150 ). Setarea acestei valori la 0 scoate din funcţiune această particularitate.',
 	'TOPIC_PREVIEW_STRIP'			=> 'Coduri BB de ascuns în previzualizarea subiectelor',
 	'TOPIC_PREVIEW_STRIP_EXPLAIN'		=> 'Afişează codurile BB cu conţinutul pe care-l doreşti înlăturat din previzualizare (spoileri şi texte cod BB ascunse, spre exemplu). Separă codurile BB multiple folosind caracterul |, de exemplu: spoiler|ascunde|cod',
 	'TOPIC_PREVIEW_AVATARS'			=> 'Display avatars in topic previews',
