b7af697e0bc1816410688c9742e3b082150048d2
cybernet <cybernet2u@users.sourceforge.net>
2014-03-25 23:59:27 +0200

    lang RO by cybernet :)

--- language/ro/info_acp_topic_preview.php
+++ language/ro/info_acp_topic_preview.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* info_acp_topic_preview [Română]
+* info_acp_topic_preview [Rom&#226;n&#259;]
 *
 * @package language
 * @copyright (c) 2014 Matt Friedman ( translated by cybernet http://xDNS.ro/ )
@@ -36,22 +36,22 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'TOPIC_PREVIEW'				=> 'Previzualizare Subiect',
-	'TOPIC_PREVIEW_EXPLAIN'			=> 'Va arăta o parte din textul primului subiect într-un tooltip în timp ce eşti cu mouse-ul deasupra titlului subiectului.',
-	'TOPIC_PREVIEW_SETTINGS'		=> 'Setari pentru previzualizarea subiectelor',
-	'TOPIC_PREVIEW_DISPLAY'			=> 'Afişează previzualizările subiectelor',
+	'TOPIC_PREVIEW_EXPLAIN'			=> 'Va ar&#259;ta o parte din textul primului subiect &#238;ntr-un tooltip &#238;n timp ce e&#351;ti cu mouse-ul deasupra titlului subiectului.',
+	'TOPIC_PREVIEW_SETTINGS'		=> 'Set&#259;ri pentru previzualizare subiecte',
+	'TOPIC_PREVIEW_DISPLAY'			=> 'Afi&#351;eaz&#259; previzualiz&#259;rile subiectelor',
 	'TOPIC_PREVIEW_LENGTH'			=> 'Lungimea textului subiectelor previzualizate',
-	'TOPIC_PREVIEW_LENGTH_EXPLAIN'		=> 'Introdu numărul de caractere ce vor fi afişate în tooltip ( valoarea implicită este 150 ). Setarea acestei valori la 0 scoate din funcţiune această particularitate.',
-	'TOPIC_PREVIEW_STRIP'			=> 'Coduri BB de ascuns în previzualizarea subiectelor',
-	'TOPIC_PREVIEW_STRIP_EXPLAIN'		=> 'Afişează codurile BB cu conţinutul pe care-l doreşti înlăturat din previzualizare (spoileri şi texte cod BB ascunse, spre exemplu). Separă codurile BB multiple folosind caracterul |, de exemplu: spoiler|ascunde|cod',
+	'TOPIC_PREVIEW_LENGTH_EXPLAIN'		=> 'Introdu num&#259;rul de caractere ce vor fi afi&#351;ate &#238;n tooltip ( valoarea implicit&#259; este 150 ). Setarea acestei valori la 0 scoate din funcţiune aceast&#259; particularitate.',
+	'TOPIC_PREVIEW_STRIP'			=> 'Coduri BB de ascuns &#238;n previzualizarea subiectelor',
+	'TOPIC_PREVIEW_STRIP_EXPLAIN'		=> 'Afi&#351;eaz&#259; codurile BB cu conţinutul pe care-l dore&#351;ti &#238;nl&#259;turat din previzualizare ( spoiler &#351;i texte cod BB ascunse, spre exemplu ). Separ&#259; codurile BB multiple folosind caracterul |, de exemplu: spoiler|ascunde|cod',
 	'TOPIC_PREVIEW_AVATARS'			=> 'Display avatars in topic previews',
-	'TOPIC_PREVIEW_LAST_POST'		=> 'Include “Last post” text in topic previews',
+	'TOPIC_PREVIEW_LAST_POST'		=> 'Include "Ultimul Post" in previzualizare',
 	'CHARS'					=> 'Caractere',
 
-	'TOPIC_PREVIEW_STYLE_SETTINGS'		=> 'Topic preview style settings',
-	'TOPIC_PREVIEW_WIDTH'			=> 'Width of topic previews ( in pixels )',
-	'TOPIC_PREVIEW_DELAY'			=> 'Delay before showing topic previews ( in milliseconds )',
-	'TOPIC_PREVIEW_DRIFT'			=> 'Animated drift effect ( in pixels )',
-	'TOPIC_PREVIEW_DRIFT_EXPLAIN'		=> 'Amount of vertical animation on fadeout (use negative values to change direction).',
+	'TOPIC_PREVIEW_STYLE_SETTINGS'		=> 'Setari de stil pentru previzualizarea subiectului',
+	'TOPIC_PREVIEW_WIDTH'			=> 'L&#259;timea previzu&#259;lizarii ( in pixeli )',
+	'TOPIC_PREVIEW_DELAY'			=> 'Am&#226;n&#259; afisarea previzualizarii ( in milliseconds )',
+	'TOPIC_PREVIEW_DRIFT'			=> 'Animated drift effect ( in pixeli )',
+	'TOPIC_PREVIEW_DRIFT_EXPLAIN'		=> 'Amount of vertical animation on fadeout ( use negative values to change direction ).',
 	'TOPIC_PREVIEW_THEME'			=> 'Stil pentru %s',
 	'TOPIC_PREVIEW_THEME_EXPLAIN'		=> 'Alege un stil pentru previzualizarea subiectului %s.',
 	'THEME'					=> 'Stil',
