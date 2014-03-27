fbcf3e349f2685c679bbfbf6083d4003fbc7c923
Matt Friedman <maf675@gmail.com>
2014-03-26 23:17:02 -0700

    Fix testing docblocks

--- tests/functional/extension_base_test.php
+++ tests/functional/extension_base_test.php
@@ -1,8 +1,8 @@
 <?php
 /**
 *
-* @package testing
-* @copyright (c) 2014 phpBB Group
+* @package Topic Preview testing
+* @copyright (c) 2014 Matt Friedman
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
--- tests/functional/topic_preview_ucp_test.php
+++ tests/functional/topic_preview_ucp_test.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package testing
+* @package Topic Preview testing
 * @copyright (c) 2014 Matt Friedman
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
