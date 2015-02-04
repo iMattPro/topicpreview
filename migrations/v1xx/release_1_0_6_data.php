5d4f71bed6a63b8c54c02ee0c74bb73017c7e150
Matt Friedman <maf675@gmail.com>
2015-02-03 22:44:32 -0800

    Add missing migration depends

--- migrations/v1xx/release_1_0_6_data.php
+++ migrations/v1xx/release_1_0_6_data.php
@@ -17,6 +17,11 @@ class release_1_0_6_data extends \phpbb\db\migration\migration
 		return isset($this->config['topic_preview_limit']);
 	}
 
+	static public function depends_on()
+	{
+		return array('\vse\topicpreview\migrations\v1xx\release_1_0_6_schema');
+	}
+
 	public function update_data()
 	{
 		return array(
--- migrations/v1xx/release_1_0_6_schema.php
+++ migrations/v1xx/release_1_0_6_schema.php
@@ -17,6 +17,11 @@ class release_1_0_6_schema extends \phpbb\db\migration\migration
 		return $this->db_tools->sql_column_exists($this->table_prefix . 'users', 'user_topic_preview');
 	}
 
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v31x\v312');
+	}
+
 	public function update_schema()
 	{
 		return array(
