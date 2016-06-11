d0916302ece8808768aaa20af5a6075d536fb695
Matt Friedman <maf675@gmail.com>
2016-06-11 10:22:38 -0700

    Fix namespace

--- controller/acp_controller_interface.php
+++ controller/acp_controller_interface.php
@@ -8,8 +8,8 @@
  * Attribution-NonCommercial-NoDerivs 3.0 Unported License.
  * http://creativecommons.org/licenses/by-nc-nd/3.0/
  */
-namespace vse\topicpreview\controller;
 
+namespace vse\topicpreview\controller;
 
 /**
  * Class acp_controller
