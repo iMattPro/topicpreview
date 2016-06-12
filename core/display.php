346149aaba60db42861d72b9c84133aaf843a280
Matt Friedman <maf675@gmail.com>
2016-06-12 08:42:17 -0700

    Docblock adjustments

--- core/display.php
+++ core/display.php
@@ -100,9 +100,9 @@ class display extends base
 		 * EVENT to modify the topic preview display output before it gets inserted in the template block
 		 *
 		 * @event vse.topicpreview.display_topic_preview
-		 * @var array row Row data
-		 * @var array block Template vars array
-		 * @var bool tp_avatars Display avatars setting
+		 * @var array row        Topic row data
+		 * @var array block      Template vars array
+		 * @var bool  tp_avatars Display avatars setting
 		 * @since 2.1.0
 		 */
 		$vars = array('row', 'block', 'tp_avatars');
